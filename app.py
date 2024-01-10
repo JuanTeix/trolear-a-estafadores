69e6f2735e7bb4df8eb44b1c864d134144280156
JTeixCode <juanteix89@hotmail.com>
2024-01-10 16:01:47 +0100

    subiendo app para trolear estafadores de whatsapp

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 # Importamos el ModuMódulo
-
+import os
+from dotenv import load_dotenv
+load_dotenv()
 import pywhatkit 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 # Usamos Un try-except
 try:
-    num_message = 10000 
+    num_message = os.getenv('NUM_MESSAGE') 
+    num_movil = os.getenv('NUM_MOVIL') 
+    message = os.getenv('MESSAGE') 
   # Enviamos el mensaje
     for i in range(num_message):
-        pywhatkit.sendwhatmsg_instantly("+34656498443",  "Mensaje De Prueba" ) 
+        pywhatkit.sendwhatmsg_instantly( num_movil ,  message ) 
     print("Mensaje Enviado " + num_message ) 
 
 except: 
